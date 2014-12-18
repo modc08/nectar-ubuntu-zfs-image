c4e8eb75c40e4ae2d6db6b7ac8cc7516b53ef724
Craig Patten <cjp@cjp.id.au>
2014-12-18 15:42:32 +1030

    Fix URL. #facepalm

--- coordinate/user-data.sh
+++ coordinate/user-data.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-wget -O - https://github.com/eResearchSA/nectar-ubuntu-zfs-image/archive/master.tar.gz | tar xzvf -
+wget -O - https://github.com/modc08/nectar-ubuntu-zfs-image/archive/master.tar.gz | tar xzvf -
 
 ( cd nectar-ubuntu-zfs-image-master/setup ; ./setup.sh )
 
